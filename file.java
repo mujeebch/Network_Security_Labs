5a7d28d6fe2b9469366cd57a437426aeafa52611
anandgarg91 <anandgarg91@gmail.com>
2016-07-05 02:03:30 +0800

    first comit

---
+++ file.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class file
+{
+public static void main (String args[]) throws FileNotFoundException
+    {
+    
+Scanner sc = new Scanner(new File(args[0]));
+int count=0;
+while(sc.hasNext()){
+    sc.next();
+    count++;
+}
+System.out.println("Number of words: " + count);
+//System.out.println(args[0]);
+}
+}
